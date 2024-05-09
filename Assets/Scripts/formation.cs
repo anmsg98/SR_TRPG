c5940702932ca3138228508b28f21b788e105d63
JeonSeongHee <wjstjd1004@naver.com>
2024-05-09 13:12:27 +0900

    [추가] 애니메이션 클립 추가

--- Assets/Scripts/formation.cs
+++ Assets/Scripts/formation.cs
@@ -46,52 +46,52 @@ public class NewBehaviourScript : MonoBehaviour
         Vector3 mousePos;
         Vector3 touchPos;
 
-        // pc 테스트
-        // if (Input.GetMouseButtonDown(0))
-        // {
-        //     mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        //     RaycastHit2D[] hits = Physics2D.RaycastAll(mousePos, transform.forward, 15f);
-        //     RaycastHit2D hit;
-        //     hit = hits[0];
-        //
-        //     if (hit.transform.tag == "Spawn_b")
-        //     {
-        //         if (camp[0].childCount < 3)
-        //         {
-        //             int idx = hit.transform.GetSiblingIndex();
-        //             GameObject obj = Instantiate(character[idx]);
-        //             obj.transform.SetParent(camp[0], false);
-        //             mousePos.z = 1f;
-        //             obj.transform.position = mousePos;
-        //             picked = true;
-        //             pickedPlayer = obj.transform;
-        //         }
-        //         else
-        //         {
-        //             alertText.text = "각 진영당 캐릭터 배치 제한은 최대 3유닛 입니다.";
-        //             alertAnim.SetTrigger("Alert_On");
-        //         }
-        //     }
-        //
-        //     else if (hit.transform.tag == "Spawn_r")
-        //     {
-        //         if (camp[1].childCount < 3)
-        //         {
-        //             int idx = hit.transform.GetSiblingIndex();
-        //             GameObject obj = Instantiate(character[idx]);
-        //             obj.transform.SetParent(camp[1], false);
-        //             mousePos.z = 1f;
-        //             obj.transform.position = mousePos;
-        //             picked = true;
-        //             pickedPlayer = obj.transform;
-        //         }
-        //         else
-        //         {
-        //             alertText.text = "각 진영당 캐릭터 배치 제한은 최대 3유닛 입니다.";
-        //             alertAnim.SetTrigger("Alert_On");
-        //         }
-        //     }
-        // }
+        //pc 테스트
+        if (Input.GetMouseButtonDown(0))
+        {
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D[] hits = Physics2D.RaycastAll(mousePos, transform.forward, 15f);
+            RaycastHit2D hit;
+            hit = hits[0];
+        
+            if (hit.transform.tag == "Spawn_b")
+            {
+                if (camp[0].childCount < 3)
+                {
+                    int idx = hit.transform.GetSiblingIndex();
+                    GameObject obj = Instantiate(character[idx]);
+                    obj.transform.SetParent(camp[0], false);
+                    mousePos.z = 1f;
+                    obj.transform.position = mousePos;
+                    picked = true;
+                    pickedPlayer = obj.transform;
+                }
+                else
+                {
+                    alertText.text = "각 진영당 캐릭터 배치 제한은 최대 3유닛 입니다.";
+                    alertAnim.SetTrigger("Alert_On");
+                }
+            }
+        
+            else if (hit.transform.tag == "Spawn_r")
+            {
+                if (camp[1].childCount < 3)
+                {
+                    int idx = hit.transform.GetSiblingIndex();
+                    GameObject obj = Instantiate(character[idx]);
+                    obj.transform.SetParent(camp[1], false);
+                    mousePos.z = 1f;
+                    obj.transform.position = mousePos;
+                    picked = true;
+                    pickedPlayer = obj.transform;
+                }
+                else
+                {
+                    alertText.text = "각 진영당 캐릭터 배치 제한은 최대 3유닛 입니다.";
+                    alertAnim.SetTrigger("Alert_On");
+                }
+            }
+        }
         
         // android 터치
         if (Input.touchCount == 1)
@@ -150,103 +150,103 @@ public class NewBehaviourScript : MonoBehaviour
         Vector3 mousePos;
         Vector3 touchPos;
         // pc 테스트
-        // if (Input.GetMouseButtonDown(0))
-        // {
-        //     mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        //     RaycastHit2D[] hits = Physics2D.RaycastAll(mousePos, transform.forward, 15f);
-        //     RaycastHit2D hit;
-        //     hit = hits[hits.Length - 1];
-        //
-        //     if (hit.transform.tag == "Player")
-        //     {
-        //         picked = true;
-        //         pickedPlayer = hit.transform;
-        //     }
-        // }
-        // else if (Input.GetMouseButton(0))
-        // {
-        //     if (picked)
-        //     {
-        //         mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        //         mousePos.z = 1f;
-        //         pickedPlayer.position = mousePos;
-        //     }
-        // }
-        // else if (Input.GetMouseButtonUp(0))
-        // {
-        //     mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        //     RaycastHit2D[] hits = Physics2D.RaycastAll(mousePos, transform.forward, 15f);
-        //     RaycastHit2D hit;
-        //     hit = hits[0];
-        //     if (picked)
-        //     {
-        //         if (hit.transform.tag == "Board")
-        //         {
-        //             // 이동한 보드 위치에 캐릭터가 없을 때
-        //             Vector2Int pos = hit.transform.GetComponent<Board>().pos;
-        //             if (board[pos.x, pos.y] == 0)
-        //             {
-        //                 // 해당 보드 위치로 옮김
-        //                 Vector3 boardPosition;
-        //                 boardPosition = hit.transform.position;
-        //                 boardPosition.z = 1f;
-        //                 pickedPlayer.position = boardPosition;
-        //
-        //                 // 캐릭터의 해당 좌표도 보드 좌표로 변경
-        //                 pos = pickedPlayer.GetComponent<Character>().pos;
-        //                 
-        //                 // 캐릭터가 처음 생성된 경우가 아닐때
-        //                 if (pos.x != -1 || pos.y != -1)
-        //                 {
-        //                     board[pos.x, pos.y] = 0;
-        //                 }
-        //
-        //                 pos = hit.transform.GetComponent<Board>().pos;
-        //                 if (pickedPlayer.GetComponent<Character>().currentState == Character.State.friend)
-        //                     board[pos.x, pos.y] = 1;
-        //                 else board[pos.x, pos.y] = 2;
-        //                 pickedPlayer.GetComponent<Character>().pos = pos;
-        //             }
-        //             // 이동한 보드 위치에 이미 캐릭터가 있을 때
-        //             else
-        //             {
-        //                 Vector2Int characterPos = pickedPlayer.GetComponent<Character>().pos;
-        //                 // 처음 생성된 캐릭터면
-        //                 if (characterPos.x == -1 && characterPos.y == -1)
-        //                 {
-        //                     Destroy(pickedPlayer.gameObject);
-        //                 }
-        //                 // 이미 배치된 캐릭터인 경우
-        //                 else
-        //                 {
-        //                     //원래 있던 보드로 옮김
-        //                     Vector2Int ps = pickedPlayer.GetComponent<Character>().pos;
-        //                     int idx = (ps.x * 10 + ps.y);
-        //                     Vector3 boardPosition;
-        //                     boardPosition = Field.GetChild(idx).transform.position;
-        //                     boardPosition.z = 1f;
-        //                     pickedPlayer.position = boardPosition;
-        //                 }
-        //
-        //                 alertText.text = "이동한 보드에 이미 캐릭터가 배치되어 있습니다.";
-        //                 alertAnim.SetTrigger("Alert_On");
-        //             }
-        //         }
-        //         else
-        //         {
-        //             // 보드 외의 위치에 놓았을 때, 캐릭터 삭제
-        //             Vector2Int pos = pickedPlayer.GetComponent<Character>().pos;
-        //             if (pos.x != -1 || pos.y != -1)
-        //             {
-        //                 board[pos.x, pos.y] = 0;
-        //             }
-        //
-        //             Destroy(pickedPlayer.gameObject);
-        //         }
-        //     }
-        //
-        //     picked = false;
-        // }
+        if (Input.GetMouseButtonDown(0))
+        {
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D[] hits = Physics2D.RaycastAll(mousePos, transform.forward, 15f);
+            RaycastHit2D hit;
+            hit = hits[hits.Length - 1];
+        
+            if (hit.transform.tag == "Player")
+            {
+                picked = true;
+                pickedPlayer = hit.transform;
+            }
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            if (picked)
+            {
+                mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                mousePos.z = 1f;
+                pickedPlayer.position = mousePos;
+            }
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D[] hits = Physics2D.RaycastAll(mousePos, transform.forward, 15f);
+            RaycastHit2D hit;
+            hit = hits[0];
+            if (picked)
+            {
+                if (hit.transform.tag == "Board")
+                {
+                    // 이동한 보드 위치에 캐릭터가 없을 때
+                    Vector2Int pos = hit.transform.GetComponent<Board>().pos;
+                    if (board[pos.x, pos.y] == 0)
+                    {
+                        // 해당 보드 위치로 옮김
+                        Vector3 boardPosition;
+                        boardPosition = hit.transform.position;
+                        boardPosition.z = 1f;
+                        pickedPlayer.position = boardPosition;
+        
+                        // 캐릭터의 해당 좌표도 보드 좌표로 변경
+                        pos = pickedPlayer.GetComponent<Character>().pos;
+                        
+                        // 캐릭터가 처음 생성된 경우가 아닐때
+                        if (pos.x != -1 || pos.y != -1)
+                        {
+                            board[pos.x, pos.y] = 0;
+                        }
+        
+                        pos = hit.transform.GetComponent<Board>().pos;
+                        if (pickedPlayer.GetComponent<Character>().currentState == Character.State.friend)
+                            board[pos.x, pos.y] = 1;
+                        else board[pos.x, pos.y] = 2;
+                        pickedPlayer.GetComponent<Character>().pos = pos;
+                    }
+                    // 이동한 보드 위치에 이미 캐릭터가 있을 때
+                    else
+                    {
+                        Vector2Int characterPos = pickedPlayer.GetComponent<Character>().pos;
+                        // 처음 생성된 캐릭터면
+                        if (characterPos.x == -1 && characterPos.y == -1)
+                        {
+                            Destroy(pickedPlayer.gameObject);
+                        }
+                        // 이미 배치된 캐릭터인 경우
+                        else
+                        {
+                            //원래 있던 보드로 옮김
+                            Vector2Int ps = pickedPlayer.GetComponent<Character>().pos;
+                            int idx = (ps.x * 10 + ps.y);
+                            Vector3 boardPosition;
+                            boardPosition = Field.GetChild(idx).transform.position;
+                            boardPosition.z = 1f;
+                            pickedPlayer.position = boardPosition;
+                        }
+        
+                        alertText.text = "이동한 보드에 이미 캐릭터가 배치되어 있습니다.";
+                        alertAnim.SetTrigger("Alert_On");
+                    }
+                }
+                else
+                {
+                    // 보드 외의 위치에 놓았을 때, 캐릭터 삭제
+                    Vector2Int pos = pickedPlayer.GetComponent<Character>().pos;
+                    if (pos.x != -1 || pos.y != -1)
+                    {
+                        board[pos.x, pos.y] = 0;
+                    }
+        
+                    Destroy(pickedPlayer.gameObject);
+                }
+            }
+        
+            picked = false;
+        }
 
         // 안드로이드 터치
         if (Input.touchCount == 1)
